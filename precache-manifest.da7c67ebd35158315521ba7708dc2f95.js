2f3fe2e031d6c2c6219691a2ea0bf9750b47bcb5
Deorigami <70903634+Deorigami@users.noreply.github.com>
2020-11-04 18:38:46 +0700

    Updates

---
+++ precache-manifest.da7c67ebd35158315521ba7708dc2f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f66c5220eb724143a75f4fdc0f952e0",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "59484b3639dc2b20308f",
+    "url": "/Weather-App/static/css/main.fda4503b.chunk.css"
+  },
+  {
+    "revision": "5e251d383ae653f17f4f",
+    "url": "/Weather-App/static/js/2.ee60fe0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.ee60fe0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59484b3639dc2b20308f",
+    "url": "/Weather-App/static/js/main.2c398957.chunk.js"
+  },
+  {
+    "revision": "4225fb8e14847625fb38",
+    "url": "/Weather-App/static/js/runtime-main.659bb6ac.js"
+  }
+]);
